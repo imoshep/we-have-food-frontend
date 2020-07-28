f0a183c438b69db5384d420dfa6477d14af119b1
Moshe Piamenta <imoshep@gmail.com>
2020-07-28 16:00:02 +0300

    Navbar completed

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,37 +1,73 @@
 import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faHome } from "@fortawesome/free-solid-svg-icons";
+import "./scss/navbar.scss";
 
 class Navbar extends Component {
-  state = {};
+  state = {
+    hideNav: true,
+  };
+
+  collapse() {
+    document.getElementById("collapse-list").classList.toggle("expand");
+  }
+
+  showNavbar() {
+    let { hideNav } = this.state;
+    let scrollBarPosition = window.pageYOffset | document.body.scrollTop;
+
+    scrollBarPosition < 60 ? (hideNav = true) : (hideNav = false);
+
+    this.setState({ hideNav });
+  }
+
+  componentDidMount() {
+    window.addEventListener("scroll", this.showNavbar.bind(this));
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("scroll", this.showNavbar.bind(this));
+  }
+
   render() {
     const { user } = this.props;
+    const { hideNav } = this.state;
     return (
-      <nav className="navbar" id="navbar">
-        <li>
-          <NavLink to="/food" className="active">
-            למצוא אוכל
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/users/my-favorites" className="active">
-            הצעות מעניינות
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/food/add" className="active">
-            שתף אוכל
-          </NavLink>
-        </li>
-        {user ? (
-          <li>
+      <nav className={`navbar ${hideNav ? "hide" : ""}`} id="navbar">
+        <ul className="top-list">
+          <li className="navlink burger">
+            <FontAwesomeIcon icon={faBars} onClick={() => this.collapse()} />
+          </li>
+          <li className="navlink home active">
+            <NavLink to="/">
+              <FontAwesomeIcon icon={faHome} />
+            </NavLink>
+          </li>
+          <span className="collapse-list" id="collapse-list">
+            <li className="navlink">
+              <NavLink to="/food">למצוא אוכל</NavLink>
+            </li>
+            <li className="navlink">
+              <NavLink to="/users/my-favorites">הצעות מעניינות</NavLink>
+            </li>
+            <li className="navlink">
+              <NavLink to="/food/add">שתף אוכל</NavLink>
+            </li>
+          </span>
+          <li className="navlink user">
             <NavLink to="/user/logout">שינוי משתמש</NavLink>
           </li>
-        ) : (
-          <li>
-            <NavLink to="/user/login">התחברות למערכת</NavLink>
-          </li>
-        )}
+          {/* {user ? (
+            <li className="navlink user">
+              <NavLink to="/user/logout">שינוי משתמש</NavLink>
+            </li>
+          ) : (
+            <li className="navlink user">
+              <NavLink to="/user/login">התחברות למערכת</NavLink>
+            </li>
+          )} */}
+        </ul>
       </nav>
     );
   }
