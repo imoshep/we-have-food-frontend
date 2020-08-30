c2443be68f4265baf3bf126b43bea89d8f651384
Moshe Piamenta <imoshep@gmail.com>
2020-08-30 12:40:45 +0300

    and again 1

--- src/components/common/forms/form.jsx
+++ src/components/common/forms/form.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Input from "./input";
 import Joi from "joi-browser";
-import { getSignedRequest } from "../../../services/foodService";
+// import { getSignedRequest } from "../../../services/foodService";
 import TextArea from "./textArea";
 import Datalist from "./datalist";
 
