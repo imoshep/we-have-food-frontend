0f7d0880a3525859e53eb9e30fe79d21ec60d9b3
Moshe Piamenta <imoshep@gmail.com>
2020-08-30 20:17:57 +0300

    building

---
+++ build/precache-manifest.6e96d0cfca4945ea89f7e883e3b99384.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa5643d697dfacb0d3e1fd41498fb068",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f849bd67f293aec186cc",
+    "url": "/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "0b8a0a2826bcfaf7c994",
+    "url": "/static/css/main.c42691f1.chunk.css"
+  },
+  {
+    "revision": "f849bd67f293aec186cc",
+    "url": "/static/js/2.f12d5331.chunk.js"
+  },
+  {
+    "revision": "eb81aabd6e84aa1fbdbc6b6b82c6cd87",
+    "url": "/static/js/2.f12d5331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b8a0a2826bcfaf7c994",
+    "url": "/static/js/main.d7928e4c.chunk.js"
+  },
+  {
+    "revision": "48791a4bb5303da13160",
+    "url": "/static/js/runtime-main.c8a44357.js"
+  },
+  {
+    "revision": "29184fdc4455d44204edd885cec8ceb3",
+    "url": "/static/media/homepage-header.29184fdc.jpg"
+  }
+]);
